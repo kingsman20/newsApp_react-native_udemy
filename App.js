89d99a675ab5ce1a03ddb4640dfd7e41f1e4fc04
kingsman20 <kingslifeudo@gmail.com>
2020-05-07 00:37:24 +0100

    completed the section on styling react native apps

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Header from './src/components/Header';
+import Card from './src/components/Card';
+
+const loadFonts = () => {
+  return Font.loadAsync({
+    'Ubuntu': require('./assets/fonts/Ubuntu-Regular.ttf'),
+    'Ubuntu-Bold': require('./assets/fonts/Ubuntu-Bold.ttf')
+  });
+}
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded) {
+    return (
+      <AppLoading 
+        startAsync={loadFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View>
       <Header />
+      <Card />
     </View>
   );
 }
