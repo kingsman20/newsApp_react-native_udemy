03a50bf273a83a0c2fc93e3cb1b462201339262c
kingsman20 <kingslifeudo@gmail.com>
2020-05-13 17:45:03 +0100

    Setup redux completed

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+import {Provider} from 'react-redux';
 
 import AppNavigator from './src/navigation/AppNavigator';
+import store from './src/redux/store';
 
 const loadFonts = () => {
   return Font.loadAsync({
@@ -26,7 +28,9 @@ export default function App() {
   }
 
   return (
-    <AppNavigator />
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
 
---
+++ src/redux/reducers/newsReducer.js
@@ -0,0 +1,18 @@
+import { FETCH_ARTICLES } from "../actions/newsAction";
+
+const initialState = {
+    articles: [],
+    favorites: []
+}
+
+export default function(state=initialState, action) {
+
+    switch(action.type) {
+        case FETCH_ARTICLES:
+            return {
+                ...state,
+                articles: action.payload
+            }
+    }
+    return state;
+}
